eb584250271d29c987d75cca3d4df6de1270ae09
Ben <bjavell15@gmail.com>
2024-11-28 22:47:00 +0800

    fix cluster for agent

--- ecosystem-agent.config.js
+++ ecosystem-agent.config.js
@@ -1,19 +1,17 @@
 module.exports = {
     apps: [
-      {
-        name: 'luckytaya-backoffice',        // Name of your application
-        script: 'npm',             // PM2 will run npm directly
-        args: 'run start:agent',   // The script to run (npm run start:admin)
-        instances: 3,              // Number of clusters (can be increased dynamically)
-        exec_mode: 'cluster',      // Run in cluster mode
-        autorestart: true,         // Automatically restart app if it crashes
-        watch: false,              // Optionally enable file watching (for development)
-        max_memory_restart: '200m',  // Optionally restart app if it exceeds 1 GB of memory
-        env: {
-          NODE_ENV: 'development'  // Environment variables, you can add more here
-        },
-        "pre-start": "npm i && npm run build:agent"
-      }
+        {
+            name: 'luckytaya-backoffice',        // Name of your application
+            script: 'npm',             // PM2 will run npm directly
+            args: 'run start:agent',   // The script to run (npm run start:admin)
+            instances: 3,              // Number of clusters (can be increased dynamically)
+            exec_mode: 'cluster',      // Run in cluster mode
+            autorestart: true,         // Automatically restart app if it crashes
+            watch: false,              // Optionally enable file watching (for development)
+            max_memory_restart: '200m',  // Optionally restart app if it exceeds 1 GB of memory
+            env: {
+                NODE_ENV: 'development'  // Environment variables, you can add more here
+            }
+        }
     ]
-  };
-  
+};

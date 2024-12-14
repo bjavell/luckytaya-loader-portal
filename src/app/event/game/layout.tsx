9326042d11b38fbab5be5f6bdee96b2be8d95215
ronald <rsmarquez@eastwestbanker.com>
2024-12-14 13:04:42 +0800

    server side game and transhistory

--- src/app/event/game/layout.tsx
+++ src/app/event/game/layout.tsx
@@ -1,12 +1,9 @@
 import { ReactNode } from "react";
 import { Metadata } from "next";
-import { WebSocketProvider } from "@/context/webSocketContext";
-import { getCurrentSession } from "@/context/auth";
 
-const GameLayout: React.FC<{ children: ReactNode }> = async ({ children }) => {
-    const session = await getCurrentSession();
-    const token = session? session.token : "";
-  return <WebSocketProvider token={token}>{children}</WebSocketProvider>;
+const GameLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+   
+  return <>{children}</>;
 };
 
 export default GameLayout;
--- src/app/event/transaction_history/layout.tsx
+++ src/app/event/transaction_history/layout.tsx
@@ -1,16 +1,12 @@
 import { ReactNode } from "react";
 import { Metadata } from "next";
-import { WebSocketProvider } from "@/context/webSocketContext";
-import { getCurrentSession } from "@/context/auth";
 
-const GameLayout: React.FC<{ children: ReactNode }> = async ({ children }) => {
-    const session = await getCurrentSession();
-    const token = session? session.token : "";
-  return <WebSocketProvider token={token}>{children}</WebSocketProvider>;
+const TransHistoryLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return <>{children}</>;
 };
 
-export default GameLayout;
+export default TransHistoryLayout;
 
 export const metadata: Metadata = {
-  title: "Game",
+  title: "Transaction History",
 };

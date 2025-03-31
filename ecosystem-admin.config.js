3c233e50f36119dc89a75f41ef9edebf0bd54c2b
bjavell <31654366+bjavell@users.noreply.github.com>
2025-03-31 01:06:57 +0000

    updaate instances

--- ecosystem-admin.config.js
+++ ecosystem-admin.config.js
@@ -4,11 +4,11 @@ module.exports = {
       name: 'luckytaya-admin',        // Name of your application
       script: 'npm',             // PM2 will run npm directly
       args: 'run start:admin',   // The script to run (npm run start:admin)
-      instances: 3,              // Number of clusters (can be increased dynamically)
+      instances: 1,              // Number of clusters (can be increased dynamically)
       exec_mode: 'cluster',      // Run in cluster mode
       autorestart: true,         // Automatically restart app if it crashes
       watch: false,              // Optionally enable file watching (for development)
-      max_memory_restart: '200M',  // Optionally restart app if it exceeds 1 GB of memory
+      //max_memory_restart: '200M',  // Optionally restart app if it exceeds 1 GB of memory
       env: {
         NODE_ENV: 'development'  // Environment variables, you can add more here
       },
